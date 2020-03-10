b05cb08833b263cb34959a552b2e516fe3104cdc
Kishan Joshi <43925446+joshi98kishan@users.noreply.github.com>
2020-03-10 11:26:35 +0530

    Added merge sort

--- sorting.cpp
+++ sorting.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include<iostream>
+#include <iostream>
 #include <chrono>
+#include <limits>
 using namespace std;
 using namespace std::chrono;
 
@@ -46,6 +47,47 @@ void selection_sort(int arr[], int len){
     }
 }
 
+void merge(int arr[], int p, int q, int r){
+    int n1 = q-p+1;
+    int n2 = r-q;
+    
+    int l1[n1+1];
+    int l2[n2+1];
+    
+    for(int i=0; i<n1; i++){
+        l1[i] = arr[p+i];
+    }
+    
+    l1[n1] = numeric_limits<int>::max();
+    
+    for(int i=0; i<n2; i++){
+        l2[i] = arr[q+1+i];
+    }
+    
+    l2[n2] = numeric_limits<int>::max();
+    
+    int i=0, j=0;
+    
+    for(int k=p; k<=r; k++){
+        if(l1[i] <= l2[j]){
+            arr[k] = l1[i];
+            i++;
+        }else{
+            arr[k] = l2[j];
+            j++;
+        }
+    }
+}
+
+void merge_sort(int arr[], int p, int r){
+    if(p<r){
+        int q = (p+r)/2;
+        merge_sort(arr, p, q);
+        merge_sort(arr, q+1, r);
+        merge(arr, p, q, r);
+    }
+}
+
 int main()
 {
     int arr[] = {45, 12, 5, 1, 0, -99, -24};
@@ -55,7 +97,8 @@ int main()
 
     // insertion_sort(arr, len);
     // bubble_sort(arr, len);
-    selection_sort(arr, len);
+    // selection_sort(arr, len);
+    merge_sort(arr, 0, len-1);
 
     auto stop = high_resolution_clock::now();
 
