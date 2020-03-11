932c763e14a5b564169163de21814fe63f9f4ed1
Kishan Joshi <43925446+joshi98kishan@users.noreply.github.com>
2020-03-11 10:24:48 +0530

    Added heap_sort algorithm

--- heap_tree.cpp
+++ heap_tree.cpp
@@ -2,43 +2,58 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
-    Node* left;
-    Node* right;
-    int data;
-
-    Node(){
-        left = NULL;
-        right = NULL;
+void max_heapify(int arr[], int heap_size, int i){
+    int largest = i;
+    
+    int left = 2*i+1;
+    int right = 2*i+2;
+    
+    if( left < heap_size && arr[left] > arr[largest]){
+        largest = left;
     }
-};
-
-class HeapTree{
-    public:
-    Node* root;
-    HeapTree(){
-        root = NUll;
+    if( right < heap_size && arr[right] > arr[largest]){
+        largest = right;
     }
-
-    void insert_node(Node* root, int new_key){
-
+    
+    if(largest != i){
+        int temp = arr[largest];
+        arr[largest] = arr[i];
+        arr[i] = temp;
+        
+        max_heapify(arr, heap_size, largest);
     }
+}
 
-    void delete_node(Node* root, int key){
-
+void build_max_heap(int arr[], int heap_size){
+    for(int i=(heap_size-1)/2; i>=0; i--){
+        max_heapify(arr, heap_size, i);
     }
+}
 
-    void search(Node* root, int key){
-
+void heap_sort(int arr[], int heap_size){
+    build_max_heap(arr, heap_size);
+    
+    int temp;
+    
+    for(int i=heap_size; i>=2; i--){
+        max_heapify(arr, i, 0);
+        
+        temp = arr[i-1];
+        arr[i-1] = arr[0];
+        arr[0] = temp;
     }
-
-};
+}
 
 int main()
 {
-    printf("Hello World");
-    Node tree = Node();
+    int arr[] = {7, 9, 3, 1, 16, 4, 2, 8, 14, 10};
+    int heap_size = 10;
+    
+    heap_sort(arr, heap_size);
 
+    for(int i=0; i<heap_size; i++){
+        cout<<arr[i]<<" ";
+    }
+    
     return 0;
 }
